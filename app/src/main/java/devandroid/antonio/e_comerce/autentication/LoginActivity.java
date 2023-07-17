e7a82506e90e0e0f085ce0ca7c020868cf4df3be
Antonio Emilio <antonio-emilio_6@outlook.com>
2023-07-17 15:00:07 -0300

    primeiro commit

---
+++ app/src/main/java/devandroid/antonio/e_comerce/autentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package devandroid.antonio.e_comerce.autentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devandroid.antonio.e_comerce.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
